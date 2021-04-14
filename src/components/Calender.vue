37f093e5d397be1e305d2965649581a4f0ace0fd
Erfan <erfan.fulldevs@gmail.com>
2021-04-14 10:48:13 +0430

    p base style added using plugin

--- src/components/Calender.vue
+++ src/components/Calender.vue
@@ -17,4 +17,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@layer base {
+  body {
+    @apply bg-bodyColor;
+  }
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,15 @@ module.exports = {
         favRose: {
           light: '#c64756',
           default: '#e84545'
-        }
+        },
+        favGreen: {
+          light: '#9ede73',
+          dark: '#184d47'
+        },
+        bodyColor: '#fad586'
+      },
+      fontSize: {
+
       }
     },
   },
@@ -21,7 +29,8 @@ module.exports = {
   plugins: [
     plugin(function ({ addBase, theme }) {
       addBase({
-        'h1': { fontSize: `${theme('fontSize.2xl')}`, fontWeight: `${theme('fontWeight.bold')}`, color: `${theme('colors.favRose.light')}` }
+        'h1': { fontSize: `${theme('fontSize.2xl')}`, fontWeight: `${theme('fontWeight.bold')}`, color: `${theme('colors.favRose.light')}` },
+        'p': { fontSize: `${theme('fontSize.xl')}`, color: `${theme('colors.favGreen.dark')}`, fontFamily: `${'fontFamily.serif'}` }
       })
     })
   ],
