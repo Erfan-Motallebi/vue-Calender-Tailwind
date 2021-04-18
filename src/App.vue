0167aef290c795df42425626c770d32d8f0e5b13
Erfan <erfan.fulldevs@gmail.com>
2021-04-18 10:55:42 +0430

    added typescript, we'll be using it when needed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
-  <calender />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<script>
-import Calender from "./components/Calender.vue";
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: "App",
+export default defineComponent({
+  name: 'App',
   components: {
-    Calender,
-  },
-};
+    HelloWorld
+  }
+});
 </script>
 
-<style></style>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
